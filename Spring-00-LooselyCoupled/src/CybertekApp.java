ce6ee37ab67952e4f932ef005e0befc6e800962d
cybertekschool <github@cybertekschool.com>
2020-10-17 11:56:17 -0400

    Looesely Coupled

--- Spring-00-LooselyCoupled/src/CybertekApp.java
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -1,13 +1,15 @@
 import implementation.Mentor;
 import service.FullTimeMentor;
 import service.MentorAccount;
+import service.PartTimeMentor;
 
 public class CybertekApp {
     public static void main(String[] args) {
 
-        FullTimeMentor fullTime = new FullTimeMentor();
+        //FullTimeMentor fullTime = new FullTimeMentor();
+        PartTimeMentor partTime = new PartTimeMentor();
 
-        MentorAccount mentor = new MentorAccount(fullTime);
+        MentorAccount mentor = new MentorAccount(partTime);
 
         mentor.manageAccount();
 
